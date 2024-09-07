9ac04ccac6f3e8b434a86b8e89b4a5bcf332d998
sathiz <work.sathiz@gmail.com>
2024-09-07 13:13:08 -0500

    final changes

--- src/main/java/com/ursaleo/health/controller/HealthCheckController.java
+++ src/main/java/com/ursaleo/health/controller/HealthCheckController.java
@@ -2,6 +2,7 @@ package com.ursaleo.health.controller;
 import com.ursaleo.health.service.CommandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,12 @@ public class HealthCheckController {
     private CommandService commandService;
 
     @GetMapping("/getPort")
-    public String getApplicationPort(@RequestParam String appName) {
-        return commandService.getApplicationPort(appName);
+    public String getApplicationPort() {
+        return commandService.getApplicationPort();
     }
 
-    @GetMapping("/executeBatchFile")
-    public String runBatchFile() {
-        return commandService.executeBatchFile();
+    @GetMapping("/executeBatchFile/{publicIp}/{privateIp}")
+    public String runBatchFile(@PathVariable String publicIp, @PathVariable String privateIp) {
+        return commandService.executeBatchFile(publicIp,privateIp);
     }
 }
--- src/main/java/com/ursaleo/health/service/CommandService.java
+++ src/main/java/com/ursaleo/health/service/CommandService.java
@@ -1,36 +1,46 @@
 package com.ursaleo.health.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 @Service
+@Slf4j
 public class CommandService {
 
     @Value("${batch.file.location}")
     private String batchFileLocation;
 
-    public String getApplicationPort(String appName) {
+    @Value("${server.app.name}")
+    private String appName;
+
+    public String getApplicationPort() {
+        String port = "";
         try {
             // Step 1: Get the PID of the application
             String pid = getPid(appName);
             if (pid == null || pid.isEmpty()) {
-                return "Application not found";
+                port = "NOT_FOUND";
+            }else{
+                // Step 2: Get the port associated with the PID
+                 port = getPorts(pid);
+                if (port == null || port.isEmpty()) {
+                    port = "Port not found for PID: "+pid;
+                }
             }
-
-            // Step 2: Get the port associated with the PID
-            String ports = getPorts(pid);
-            if (ports == null || ports.isEmpty()) {
-                return "No ports found for PID: " + pid;
-            }
-
-            return ports;
         } catch (Exception e) {
-            e.printStackTrace();
-            return "Error occurred: " + e.getMessage();
+            log.error(e.getMessage(),e);
+            port = "Exception when getting port : "+e.getMessage();
         }
+
+        return port;
     }
 
     private String getPid(String appName) throws Exception {
@@ -51,23 +61,33 @@ public class CommandService {
         String command = "cmd.exe /c for /f \"tokens=2 delims=:\" %i in ('netstat -ano ^| findstr /r /c:\"TCP.*" + pid + "\"') do @echo %i | for /f \"tokens=1 delims= \" %j in ('more') do @echo %j";
         Process portProcess = Runtime.getRuntime().exec(command);
         BufferedReader portReader = new BufferedReader(new InputStreamReader(portProcess.getInputStream()));
-        StringBuilder ports = new StringBuilder();
         String portLine;
 
         while ((portLine = portReader.readLine()) != null) {
             return portLine.trim();
         }
 
-        return null; // or return an appropriate message if no port is found
+        return null;
     }
 
-    public String executeBatchFile() {
+    public String executeBatchFile(String publicIp, String privateIp) {
         try {
-            String[] command = {"cmd.exe", "/c", "start", batchFileLocation};
-            Process process = Runtime.getRuntime().exec(command);
+
+         /*   String[] command = {"cmd.exe", "/K", "start.bat", publicIp, privateIp};
+            log.info("Executing command {}", Arrays.toString(command));
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.directory(new File(batchFileLocation));
+            processBuilder.start();*/
+
+            CommandLine cmd = new CommandLine("cmd.exe ");
+            cmd.addArgument("/c");
+            String command = String.format("cmd.exe /K start.bat %s %s",publicIp, privateIp);
+            cmd.addArgument(command,false);
+            DefaultExecutor.builder().setWorkingDirectory(new File(batchFileLocation)).get().execute(cmd);
             return "SUCCESS";
+
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error executing batch file: {}", e.getMessage(), e);
             return "Error executing batch file: " + e.getMessage();
         }
     }
